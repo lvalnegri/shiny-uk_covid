8e4e0d4125282100dcbf612adde8fd53e22aae70
Luca Valnegri <l.valnegri@datamaps.co.uk>
2020-04-18 00:50:52 +0100

    added cases using UTLA file from gov dashboard

--- data_downloading_age+trusts.R
+++ data_downloading_age+trusts.R
@@ -1,7 +1,8 @@
-#################################################################################
-# CoViD-19 UK * NHS Deaths by both reporting and death dates * Data Downloading #
-#################################################################################
+###########################################################################################################
+# CoViD-19 UK * NHS Cases by UTLA and reporting date + Deaths by TRUSTS by both reporting and death dates #
+###########################################################################################################
 # https://www.england.nhs.uk/statistics/statistical-work-areas/covid-19-daily-deaths/
+# https://coronavirus.data.gov.uk/
 
 pkg <- c('popiFun', 'data.table', 'fst', 'readxl')
 invisible(lapply(pkg, require, char = TRUE))
@@ -9,7 +10,7 @@ invisible(lapply(pkg, require, char = TRUE))
 in_path <- file.path(pub_path, 'ext_data', 'uk', 'covid')
 out_path <- file.path(pub_path, 'datasets', 'shiny_apps', 'uk_covid')
 
-# Class Age
+# Deaths by Age Class
 fn <- file.path(in_path, 'age_data.xlsx')
 sns <- excel_sheets(fn)
 y <- list()
@@ -26,7 +27,7 @@ y[, age := factor(gsub('[ [:alpha:]]', '', age))]
 setcolorder(y, c('date_reported', 'date_happened'))
 write_fst(y, file.path(out_path, 'age_data'))
 
-# Trusts
+# Deaths by Trusts
 fn <- file.path(in_path, 'trust_data.xlsx')
 sns <- excel_sheets(fn)
 y <- list()
@@ -41,6 +42,19 @@ for(sn in sns){
 }
 y[, trust := factor(trust)]
 write_fst(y, file.path(out_path, 'trust_data'))
+
+# Cases by UTLA England
+yu <- fread(file.path(in_path, 'coronavirus-cases.csv'))
+yu[, c('Area name', 'Area type', 'Cumulative lab-confirmed cases') := NULL]
+names(yu) <- c('UTLA', 'date_reported', 'cases')
+lcns <- readRDS(file.path(out_path, 'locations'))
+yu <- yu[lcns[['UTLA']][, .(UTLA, RGN)], on = 'UTLA']
+yu[UTLA == 'E09000001' & date_reported == '2020-03-20', cases := 5]
+yu[UTLA == 'E09000012' & date_reported == '2020-03-20', cases := 9]
+yr <- yu[, .(cases = sum(cases)), .(date_reported, RGN)]
+yu[, RGN := NULL]
+y <- list('UTLA' = yu, 'RGN' = yr)
+saveRDS(y, file.path(out_path, 'cases'))
 
 # exit
 rm(list = ls())
