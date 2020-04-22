3b9c4b9b62e27591cd3f39b089499bfa94d525af
Luca Valnegri <l.valnegri@datamaps.co.uk>
2020-04-22 01:45:24 +0100

    first try to add "plots" on popup for trusts

--- single_leaflet_map-UTLA+STP+TRUSTS.R
+++ single_leaflet_map-UTLA+STP+TRUSTS.R
@@ -3,11 +3,13 @@
 ####################################
 
 # load libraries
-pkg <- c('popiFun', 'data.table', 'fst', 'htmltools', 'htmlwidgets', 'leaflet', 'leaflet.extras')
+pkg <- c('popiFun', 'data.table', 'fst', 'ggplot2', 'gridExtra', 'htmltools', 'htmlwidgets', 'leaflet', 'leaflet.extras', 'leafpop')
 invisible(lapply(pkg, require, char = TRUE))
 
-# set constant
+# set constants
 app_path <- file.path(pub_path, 'datasets', 'shiny_apps', 'uk_covid')
+r_min <- 10
+r_max <- 75
 
 # options for map hover labels
 lbl.options <- labelOptions(
@@ -67,7 +69,9 @@ add_label_poly <- function(y, type){
                     ),
                     't' = paste0(
                             '<hr>',
-                                '<b>Trust</b>: ', y$name[x], '<br>',
+                                '<b>', ifelse(y$type[x] == 'P', 'Partner', 
+                                            ifelse(y$type[x] == 'T', 'Trust', 'Foundation Trust')), 
+                                                '</b>: ', y$name[x], '<br>',
                                 '<b>CCG</b>: ', y$CCG_name[x], '<br>',
                                 '<b>STP</b>: ', y$STP_name[x], '<br>',
                                 '<b>NHS Region</b>: ', y$NHSR_name[x], '<br>',
@@ -89,7 +93,7 @@ yt <- readRDS(file.path(app_path, 'locations'))
 yt <- yt[['trusts']]
 dts.t <- read_fst(file.path(app_path, 'trust_data'), as.data.table = TRUE)
 yt <- dts.t[is.na(date_reported), .(deaths = sum(N)), .(code = trust)][yt, on = 'code'][is.na(deaths), deaths := 0]
-q75 <- as.numeric(quantile(yt$deaths, 0.75))
+yt[deaths > 0, radius := sqrt(deaths) - 1][, radius := (radius/max(radius, na.rm = TRUE)) * (r_max - r_min) + r_min]
 
 # read UTLA cases data
 dts.u <- readRDS(file.path(app_path, 'cases'))
@@ -119,6 +123,48 @@ bnd.stp$pdeaths <- round(bnd.stp$deaths * 1000000 / bnd.stp$popT)
 pal.deaths <- colorQuantile('YlOrRd', bnd.stp$deaths, 9)
 pal.pdeaths <- colorQuantile('YlOrRd', bnd.stp$pdeaths, 9)
 
+### create plots (save in subfolder "plots")
+
+## A) UTLA
+
+
+## B) STP
+
+
+## C) TRUSTS
+
+for(idx in 1:nrow(yt)){
+    
+    trs <- yt[idx, code]
+    message('Processing trust ', trs, ' (', idx, ' out of ', nrow(yt), ')')
+    
+    if(yt[idx, deaths] > 0){
+        
+        # cumulative deaths as line on left axis + daily deaths as bars on right axis
+        dt <- dts.t[is.na(date_reported) & trust == trs, .(date_happened, N)][order(date_happened)][, CN := cumsum(N)][CN > 0]
+        dt[, lbl := N][N <= 1, lbl := NA]
+        gp1 <- ggplot(dt, aes(date_happened)) +
+                    geom_col(aes(y = N), fill = 'red') +
+                    geom_text(aes(y = lbl, label = N), vjust = 1.4, color = 'black', size = 3) +
+                    geom_line(aes(y = CN, group = 1), color = 'blue') +
+                    geom_text(aes(y = CN, label = CN), hjust = 0.8, vjust = -1, color = 'blue', size = 2.5) +
+                    labs(title = 'Cumulative and Daily Deaths by Date of Death', x = '', y = '') +
+                    theme_minimal()
+        
+        # histogram for reporting delay
+        dt <- dts.t[!is.na(date_reported) & !is.na(delay) & trust == trs][, .N, delay]
+        gp2 <- ggplot(dt, aes(delay, N)) +
+                    geom_col(colour = 'black', fill = 'white') +
+                    labs(title = 'Delay in Days between Death and Report', x = '', y = '') +
+                    theme_minimal()
+        
+        # save plot as image
+        ggsave(paste0('./plots/trust_', trs, '.png'), grid.arrange(gp1, gp2, nrow = 2), 'png', width = 31.75, height = 20.11, units = 'cm')
+        
+    }
+
+}
+    
 # base layer
 mp <- leaflet(options = leafletOptions(minZoom = 6)) %>% 
         # addProviderTiles(providers$Esri.WorldTopoMap) %>% 
@@ -231,45 +277,85 @@ mp <- mp %>%
         data = yt0,
         group = grp.y0,
         lng = ~x_lon, lat = ~y_lat,
-        radius = 6,
         color = 'black', 
-        weight = 2,
+        weight = 1,
+        radius = 6,
         fillColor = 'green', 
-        fillOpacity = 0.7,
+        fillOpacity = 0.6,
         label = add_label_poly(yt0, 't'),
         labelOptions = lbl.options
     )
 
-# check choices (TRUSTS): 2) death < threshold 
-yt1 <- yt[deaths > 0 & deaths < q75]
-grp.y1 <- paste0('Hospitals Deaths Less Than ', q75, ' (', nrow(yt1), ')')
+# check choices (TRUSTS): 2) death less than or equal to Q1 (#FCC5C0)
+yt1 <- yt[deaths > 0 & deaths <= fivenum(yt$deaths)[2]]
+grp.y1 <- paste0('Hospitals Deaths Less Than or Equal to Q1 = ', fivenum(yt$deaths)[2], ' (', nrow(yt1), ')')
 mp <- mp %>% 
     addCircleMarkers(
         data = yt1,
         group = grp.y1,
         lng = ~x_lon, lat = ~y_lat,
-        color = 'black', 
+        radius = ~radius,
+        fillColor = '#FCC5C0', 
+        fillOpacity = 0.6,
         weight = 1,
-        fillColor = 'red', 
-        fillOpacity = 0.7,
+        color = 'black',
         label = add_label_poly(yt1, 't'),
-        labelOptions = lbl.options
-    )
+        labelOptions = lbl.options,
+        popup = ~popupImage(paste0('/srv/shiny-server/uk_covid/plots/trust_', code, '.png'), src = 'local')
+)
 
-# check choices (TRUSTS): 3) death >= threshold 
-yt2 <- yt[deaths >= q75]
-grp.y2 <- paste0('Hospitals Deaths More Than ', q75, ' (', nrow(yt1), ')')
+# check choices (TRUSTS): 3) death between Q1 + 1 and Q2 = Med (#F768A1)
+yt2 <- yt[deaths > fivenum(yt$deaths)[2] & deaths <= fivenum(yt$deaths)[3]]
+grp.y2 <- paste0('Hospitals Deaths Between (Q1 + 1) = ', (fivenum(yt$deaths)[2] + 1), ' and Md = ', fivenum(yt$deaths)[3], ' (', nrow(yt2), ')')
 mp <- mp %>% 
     addCircleMarkers(
         data = yt2,
         group = grp.y2,
         lng = ~x_lon, lat = ~y_lat,
-        color = 'black', 
+        radius = ~radius,
+        fillColor = '#F768A1', 
+        fillOpacity = 0.6,
         weight = 1,
-        fillColor = 'red', 
-        fillOpacity = 0.7,
+        color = 'black', 
         label = add_label_poly(yt2, 't'),
-        labelOptions = lbl.options
+        labelOptions = lbl.options,
+        popup = ~popupImage(paste0('/srv/shiny-server/uk_covid/plots/trust_', code, '.png'), src = 'local')
+    )
+
+# check choices (TRUSTS): 4) death between Med + 1 and Q3 (#DD3497)
+yt3 <- yt[deaths > fivenum(yt$deaths)[3] & deaths <= fivenum(yt$deaths)[4]]
+grp.y3 <- paste0('Hospitals Deaths Between (Md + 1) = ', (fivenum(yt$deaths)[3] + 1), ' and Q3 = ', fivenum(yt$deaths)[4], ' (', nrow(yt3), ')')
+mp <- mp %>% 
+    addCircleMarkers(
+        data = yt3,
+        group = grp.y3,
+        lng = ~x_lon, lat = ~y_lat,
+        radius = ~radius,
+        fillColor = '#DD3497', 
+        fillOpacity = 0.6,
+        weight = 1,
+        color = 'black', 
+        label = add_label_poly(yt3, 't'),
+        labelOptions = lbl.options,
+        popup = ~popupImage(paste0('/srv/shiny-server/uk_covid/plots/trust_', code, '.png'), src = 'local')
+    )
+
+# check choices (TRUSTS): 5) death greater than Q3 + 1 (#7A0177)
+yt4 <- yt[deaths > fivenum(yt$deaths)[4]]
+grp.y4 <- paste0('Hospitals Deaths More Than (Q3 + 1) = ', (fivenum(yt$deaths)[4] + 1), ' (', nrow(yt4), ')')
+mp <- mp %>% 
+    addCircleMarkers(
+        data = yt4,
+        group = grp.y4,
+        lng = ~x_lon, lat = ~y_lat,
+        radius = ~radius,
+        fillColor = '#7A0177', 
+        fillOpacity = 0.6,
+        weight = 1,
+        color = 'black', 
+        label = add_label_poly(yt4, 't'),
+        labelOptions = lbl.options,
+        popup = ~popupImage(paste0('/srv/shiny-server/uk_covid/plots/trust_', code, '.png'), src = 'local')
     )
 
 # Add upper right menu for controls
@@ -279,7 +365,7 @@ mp <- mp %>%
     	    'UTLA Clear', 'UTLA Cases', 'UTLA Cases vs 1mln Population',
     	    'STP Clear', 'STP Deaths', 'STP Deaths vs 1mln Population'
     	),
-        overlayGroups = c(grp.y0, grp.y1, grp.y2),
+        overlayGroups = c(grp.y0, grp.y1, grp.y2, grp.y3, grp.y4),
     	options = layersControlOptions(collapsed = FALSE)
     ) 
 
